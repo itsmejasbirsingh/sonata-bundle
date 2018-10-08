5f611c9ba890eea466292505de123d1345dd314d
itsmejasbirsingh <itsmejasbirsingh@gmail.com>
2018-10-08 18:34:01 +0530

    sonata bundle

---
+++ src/Admin/CategoryAdmin.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class CategoryAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('name', TextType::class);
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('name');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('name');
+    }
+    public function toString($object)
+            {
+                return $object instanceof Category
+                    ? $object->getName()
+                    : 'Category'; // shown in the breadcrumb on the create view
+            }
+}
